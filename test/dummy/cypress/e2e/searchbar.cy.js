5d10f743fa26981722de25dee1226703d10ff80d
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-10-14 17:31:47 +0100

    Cypress test

---
+++ test/dummy/cypress/e2e/searchbar.cy.js
@@ -0,0 +1,9 @@
+describe('Searchbar Visibility', () => {
+  it('should not display searchbar by default', () => {
+    cy.visit('/')
+    
+    // Assert searchbar is not present by default
+    cy.get('.hdi-search-form').should('not.exist')
+    cy.get('#content-header').should('not.exist')
+  })
+})
