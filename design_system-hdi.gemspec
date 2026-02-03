f30706e821a335e1986e71d83a40483e759053e3
FilisLiu <138935059+FilisLiu@users.noreply.github.com>
2026-02-03 17:56:00 +0000

    HDI action link (#18)
    * ds latest version
    
    * gemlock
    
    * hdi action link
    
    * hdi scss
    
    * dummy app demo
    
    * test
    
    * custom color
    
    * changelog
    
    * supress rubocop warning on svg line too long

--- design_system-hdi.gemspec
+++ design_system-hdi.gemspec
@@ -22,7 +22,7 @@ Gem::Specification.new do |spec|
     Dir['{app,config,db,lib,public}/**/*', 'MIT-LICENSE', 'Rakefile', 'README.md']
   end
 
-  spec.add_dependency 'design_system', '~> 0.11.0'
+  spec.add_dependency 'design_system', '~> 0.12.0'
   spec.add_dependency 'rails', '>= 7.1.5.2'
 
   spec.metadata['rubygems_mfa_required'] = 'true'
---
+++ lib/design_system/hdi/builders/action_link.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module DesignSystem
+  module Hdi
+    module Builders
+      # This class provides HDI Action Link.
+      class ActionLink < ::DesignSystem::Nhsuk::Builders::ActionLink
+        private
+
+        # rubocop:disable Rails/OutputSafety, Layout/LineLength
+        def arrow_right_circle_icon
+          %(
+            <svg class="#{brand}-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+          ).html_safe
+        end
+        # rubocop:enable Rails/OutputSafety, Layout/LineLength
+      end
+    end
+  end
+end
