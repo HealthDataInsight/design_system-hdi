a20ac74cca4312b8496ea0329838fa68bc25b4bd
FilisLiu <138935059+FilisLiu@users.noreply.github.com>
2026-02-06 16:32:01 +0000

    HDI backlink ds helper (#20)
    * Add backlink yield to HDI application layout
    
    * demo
    
    * Add Backlink module to HDI fixed elements builder
    
    * update hdi stylesheet
    
    * Add tests for HDI backlink builder functionality
    
    * linting

--- lib/design_system/hdi/builders/fixed_elements.rb
+++ lib/design_system/hdi/builders/fixed_elements.rb
@@ -5,6 +5,7 @@ module DesignSystem
     module Builders
       # This class is used to provide HDI fixed elements builder.
       class FixedElements < ::DesignSystem::Generic::Builders::FixedElements
+        include Elements::Backlink
         include Elements::Breadcrumbs
         include Elements::Headings
       end
