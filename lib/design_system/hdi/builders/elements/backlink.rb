cfde49c6b86acf02eb5f091980df18b68bebfa49
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-02-06 14:35:35 +0000

    linting

--- lib/design_system/hdi/builders/elements/backlink.rb
+++ lib/design_system/hdi/builders/elements/backlink.rb
@@ -5,7 +5,7 @@ module DesignSystem
         # Backlink should not coexist with breadcrumbs.
         module Backlink
           include ::DesignSystem::Generic::Builders::Elements::Backlink
-          
+
           # rubocop:disable Rails/OutputSafety
           BACKLINK_LEFT_CHEVRON_SVG = <<~SVG.html_safe
             <svg class="hdi-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="12 0 12 24">
@@ -26,5 +26,3 @@ module DesignSystem
     end
   end
 end
-
-
