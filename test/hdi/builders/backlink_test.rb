e119bef2ff848192f4e71ce43555103c4728615b
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-07-20 11:52:20 +0100

    Refactor HDI fixed elements: remove Backlink and Headings modules from FixedElements class, update SCSS for backlink styling, and adjust tests to reflect changes in backlink rendering.

--- test/hdi/builders/backlink_test.rb
+++ test/hdi/builders/backlink_test.rb
@@ -19,9 +19,7 @@ module DesignSystem
           end
 
           @output_buffer = @view_flow.get(:backlink)
-          assert_select("a.#{@brand}-back-link", href: assistant_path(@assistant), text: 'Back') do
-            assert_select("svg.#{@brand}-icon")
-          end
+          assert_select("a.#{@brand}-back-link", href: assistant_path(@assistant), text: 'Back')
         end
 
         test 'rendering hdi backlink only, with label' do
@@ -30,9 +28,7 @@ module DesignSystem
           end
 
           @output_buffer = @view_flow.get(:backlink)
-          assert_select("a.#{@brand}-back-link", href: assistant_path(@assistant), text: 'Custom text') do
-            assert_select("svg.#{@brand}-icon")
-          end
+          assert_select("a.#{@brand}-back-link", href: assistant_path(@assistant), text: 'Custom text')
         end
 
         test 'rendering hdi backlink with breadcrumbs raises error' do
