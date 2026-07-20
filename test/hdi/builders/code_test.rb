6ddede448bb209fdda175b5243f11d237c96f6aa
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-07-20 11:36:56 +0100

    Add HDI code rendering functionality: implement code block display with clipboard copy feature and update tests accordingly.

--- test/hdi/builders/code_test.rb
+++ test/hdi/builders/code_test.rb
@@ -8,6 +8,7 @@ module DesignSystem
       # This tests the hdi code builder
       class CodeTest < ActionView::TestCase
         include DesignSystemHelper
+        include HdiHelper
 
         setup do
           @brand = 'hdi'
@@ -17,9 +18,15 @@ module DesignSystem
         test 'rendering hdi code block' do
           @output_buffer = ds_code('puts "hello"', 'ruby')
 
-          assert_select 'div.app-example__code' do
-            assert_select 'button.app-example__copy-button', text: 'Copy'
-            assert_select 'pre code.hljs.language-ruby', text: 'puts "hello"'
+          assert_select 'div.hdi-code-container' do
+            assert_select 'div.hdi-code-heading' do
+              assert_select 'span', text: 'Ruby'
+              assert_select 'button.hdi-clipboard-button' do
+                assert_select 'img.hdi-clipboard-button__icon'
+                assert_select 'span.hdi-clipboard-button__label', text: 'Copy'
+              end
+            end
+            assert_select 'pre.hdi-code-console code.hljs.language-ruby', text: 'puts "hello"'
           end
         end
       end
