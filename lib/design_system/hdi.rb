2b06721e20701e86abe78fe5ef4831828634cfd2
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-07-20 12:14:51 +0100

    zeitwerk

--- lib/design_system/hdi.rb
+++ lib/design_system/hdi.rb
@@ -9,19 +9,4 @@ require 'zeitwerk'
 loader = Zeitwerk::Loader.for_gem_extension(DesignSystem)
 loader.setup
 
-require 'design_system/hdi/builders/button'
-require 'design_system/hdi/builders/code'
-require 'design_system/hdi/builders/fixed_elements'
-require 'design_system/hdi/builders/grid'
-require 'design_system/hdi/builders/inset_text'
-require 'design_system/hdi/builders/link'
-require 'design_system/hdi/builders/notification'
-require 'design_system/hdi/builders/pagination_renderer'
-require 'design_system/hdi/builders/paragraph'
-require 'design_system/hdi/builders/summary_list'
-require 'design_system/hdi/builders/tab'
-require 'design_system/hdi/builders/table'
-
-require 'design_system/hdi/form_builder'
-
 DesignSystem::Registry.register('hdi')
